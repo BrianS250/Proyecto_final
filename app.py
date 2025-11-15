4838c3dab889b030777d758aebcf01bb6667b3ee
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-15 13:22:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-#app.py
+# app.py
 import streamlit as st
-from modulos.venta import mostrar_venta # Importamos la función mostrar_venta del módulo venta
-from modulos.login import login 
-# Llamamos a la función mostrar_venta para mostrar el mensaje de la app
-mostrar_venta() 
-login() 
+from modulos.login import login
+from modulos.venta import mostrar_venta
+
+# Verificamos si la sesión ya está iniciada
+if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
+    st.sidebar.title("Menú principal")
+    opciones = ["Ventas", "Cerrar sesión"]
+    seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
+
+    if seleccion == "Ventas":
+        mostrar_venta()
+
+    elif seleccion == "Cerrar sesión":
+        st.session_state["sesion_iniciada"] = False
+        st.success("Sesión cerrada correctamente.")
+        st.rerun()
+
+else:
+    # Mostrar el login si no hay sesión activa
+    login()
+
 
