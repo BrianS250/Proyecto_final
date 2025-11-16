9dc607e64ea07ccc5e508e4765e6bc9246ec4935
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-15 20:36:36 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -3,24 +3,20 @@ from modulos.conexion import obtener_conexion
 
 def login():
     st.title("🔐 Iniciar sesión")
-
     usuario = st.text_input("Usuario")
     contra = st.text_input("Contraseña", type="password")
 
     if st.button("Iniciar sesión"):
         con = obtener_conexion()
-        cursor = con.cursor()
-
-        cursor.execute(
-            "SELECT Usuario, Contra, Rol FROM Empleado WHERE Usuario=%s AND Contra=%s",
-            (usuario, contra)
-        )
-        fila = cursor.fetchone()
+        cur = con.cursor()
+        cur.execute("SELECT Usuario, Contra, Rol FROM Empleado WHERE Usuario=%s AND Contra=%s", (usuario, contra))
+        fila = cur.fetchone()
 
         if fila:
             st.session_state["sesion_iniciada"] = True
+            st.session_state["usuario"] = fila[0]
             st.session_state["rol"] = fila[2]
             st.success(f"✅ Bienvenido, {fila[0]} ({fila[2]})")
-            st.rerun()
         else:
             st.error("❌ Usuario o contraseña incorrectos")
+        con.close()
