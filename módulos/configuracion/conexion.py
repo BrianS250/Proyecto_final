d4f1b5136f659b7d327681b57af1a31ebec0dad1
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-15 19:47:57 -0600

    Delete módulos/configuracion directory

--- módulos/configuracion/conexion.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def obtener_conexion():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",           # tu usuario MySQL
-        password="",           # si tienes contraseña, ponla aquí
-        database="btcfcbzptdyxq4f8afmu"  # nombre exacto de tu BD
-    )
