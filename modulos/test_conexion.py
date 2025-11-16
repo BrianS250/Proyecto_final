afe5c6fbc575968e58c2a01dd14dfb3a48b7576a
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-16 12:24:13 -0600

    Update test_conexion.py

--- modulos/test_conexion.py
+++ modulos/test_conexion.py
@@ -1,8 +1,24 @@
+import streamlit as st
 from modulos.conexion import obtener_conexion
 
-try:
+def probar_conexion():
+    st.title("🔌 Test de conexión a MySQL (Clever Cloud)")
+
     con = obtener_conexion()
-    print("✅ Conexión exitosa a la base de datos")
-    con.close()
-except Exception as e:
-    print("❌ Error:", e)
+
+    if con:
+        st.success("✅ Conexión exitosa con Clever Cloud")
+
+        try:
+            cursor = con.cursor()
+            cursor.execute("SELECT 1;")
+            resultado = cursor.fetchone()
+            st.write("Resultado de prueba:", resultado)
+        except Exception as e:
+            st.error(f"⚠️ La conexión se abrió, pero hubo un error al ejecutar una consulta: {e}")
+    else:
+        st.error("❌ No se pudo conectar. Revisa tus credenciales.")
+
+probar_conexion()
+
+
