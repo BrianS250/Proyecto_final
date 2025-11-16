48c47218d43818310e3e3332d88d67f9fd9fd7f4
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-15 20:33:51 -0600

    Update conexion.py

--- modulos/conexion.py
+++ modulos/conexion.py
@@ -2,10 +2,13 @@ import mysql.connector
 
 def obtener_conexion():
     return mysql.connector.connect(
-        host="bftcfbzptdyxq4f8afmu-mysql.services.clever-cloud.com",  # tu host en Clever Cloud
-        user="uXXXXX",       # tu usuario en Clever Cloud
-        password="pXXXXX",   # tu contraseña
-        database="btfcfbzptdyxq4f8afmu"
+        host="bftcfbzptdyxq4f8afmu-mysql.services.clever-cloud.com",  # Host de Clever Cloud
+        user="uXXXXXX",        # Tu usuario real de Clever Cloud
+        password="pXXXXXX",    # Tu contraseña real
+        database="btfcfbzptdyxq4f8afmu",  # Nombre exacto de la base
+        port=3306              # Puerto estándar de MySQL
+    )
+
     )
 
 
