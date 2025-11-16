23c9dd08177f544c8d6d12a0cbb0d4834fa63572
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-15 20:24:29 -0600

    Update promotora.py

--- modulos/promotora.py
+++ modulos/promotora.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from modulos.Configuración.conexion import obtener_conexion
-
+from modulos.conexion import obtener_conexion
 
 def interfaz_promotora():
     st.header("👩‍💼 Panel de Promotora")
@@ -8,12 +7,13 @@ def interfaz_promotora():
     con = obtener_conexion()
     cursor = con.cursor()
 
-    cursor.execute("SELECT Id_Multa, Monto, Estado FROM Multa")
+    cursor.execute("SELECT Id_Multa, Monto, Estado FROM Multa WHERE Estado='Pendiente'")
     multas = cursor.fetchall()
 
-    st.subheader("📌 Multas registradas")
+    st.subheader("📌 Multas Pendientes")
     if multas:
         for multa in multas:
             st.write(f"ID: {multa[0]} | Monto: ${multa[1]} | Estado: {multa[2]}")
     else:
-        st.info("No hay multas disponibles por el momento.")
+        st.info("No hay multas pendientes por el momento.")
+
