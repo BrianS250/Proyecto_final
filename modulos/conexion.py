01e75ad4a3ae75ad2f72ef2ac0b74774756bd3cd
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-17 20:36:31 -0600

    Update conexion.py

--- modulos/conexion.py
+++ modulos/conexion.py
@@ -3,17 +3,17 @@ import streamlit as st
 
 def obtener_conexion():
     try:
-        conexion = mysql.connector.connect(
+        return mysql.connector.connect(
             host="btcfcbzptdyxq4f8afmu-mysql.services.clever-cloud.com",
-            user="unruixx62rfqfqi5",
-            password="tHsn5wIjxSzedGOsZmtL",
+            user="up9rtj9bpwrxagre",
+            password="pnuW9k8GsvbX69sAYp7m",
             database="btcfcbzptdyxq4f8afmu",
             port=3306
         )
-        return conexion
-    except mysql.connector.Error as e:
-        st.error(f"❌ Error al conectar con la base de datos: {e}")
+    except Exception as e:
+        st.error(f"Error al conectar con la base de datos: {e}")
         return None
+
 
 
 
