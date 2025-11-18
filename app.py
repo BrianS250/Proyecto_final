338845df9083f71dd78418ed6ed18ba6c2c52e95
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-18 16:41:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,21 @@
 import streamlit as st
-
 from modulos.login import login
 from modulos.directiva import interfaz_directiva
-from modulos.promotora import interfaz_promotora
-# Cuando agregues administrador:
-# from modulos.administrador import interfaz_admin
 
-# ----------------------------------------------
-# ESTADOS DE SESIÓN
-# ----------------------------------------------
+# ---------------------------------------------------
+# ESTADO DE SESIÓN PARA CONTROLAR LOGIN / LOGOUT
+# ---------------------------------------------------
 
 if "sesion_iniciada" not in st.session_state:
     st.session_state["sesion_iniciada"] = False
 
-if "rol" not in st.session_state:
-    st.session_state["rol"] = None
-
-# ----------------------------------------------
-# REDIRECCIÓN POR ROL
-# ----------------------------------------------
+# ---------------------------------------------------
+# MOSTRAR PANTALLA SEGÚN SESIÓN
+# ---------------------------------------------------
 
 if st.session_state["sesion_iniciada"]:
-
-    if st.session_state["rol"] == "directiva":
-        interfaz_directiva()
-
-    elif st.session_state["rol"] == "promotora":
-        interfaz_promotora()
-
-    elif st.session_state["rol"] == "admin":
-        st.write("Panel de administrador (falta agregar módulo).")
-
-    else:
-        st.error("⚠ Rol no reconocido.")
-
+    # Si la sesión está activa -> cargar menú de directiva
+    interfaz_directiva()
 else:
+    # Si no ha iniciado sesión -> mostrar login
     login()
