bb01612b86c6223d2bcc945ee48bc77b8d8695f6
BrandonLABSGI <00202521@uca.edu.sv>
2025-11-19 19:30:31 -0600

    Update conexion.py

--- modulos/conexion.py
+++ modulos/conexion.py
@@ -5,16 +5,16 @@ def obtener_conexion():
     try:
         return mysql.connector.connect(
             host="btcfcbzptdyxq4f8afmu-mysql.services.clever-cloud.com",
-            user="unruixx62rfqfqi5",
-            password="tHsn5wIjxSzedGOsZmtL",
-            database="btcfcbzptdyxq4f8afmu"
+            port=3306,
+            user="unruvixx62rfxqfi5",
+            password="thsm5lvj3szedOcs1mIzL",
+            database="btcfcbzptdyxq4f8afmu",
+            ssl_ca=None,              # <--- si tu BD NO requiere SSL
+            ssl_disabled=True         # <--- fuerza conexión sin SSL
         )
     except Exception as e:
-        st.error(f"Error al conectar con la base de datos: {e}")
+        st.error(f"❌ Error al conectar con la base de datos: {e}")
         return None
-
-
-
 
 
 
