26ca3cfc6d1dcc30012fc4d1927a8ca5fb2021dd
AmilcarRodriguez44 <00043319@uca.edu.sv>
2025-11-21 20:10:14 -0600

    Update directiva.py

--- modulos/directiva.py
+++ modulos/directiva.py
@@ -117,7 +117,7 @@ def pagina_multas():
     # ============================================================
     st.subheader("➕ Registrar nueva multa")
 
-    cursor.execute("SELECT Id_Socia, Nombre FROM Socia ORDER BY Nombre ASC")
+   cursor.execute("SELECT Id_Socia, Nombre FROM Socia ORDER BY Id_Socia ASC")
     socias = cursor.fetchall()
     opciones_socias = {s["Nombre"]: s["Id_Socia"] for s in socias}
 
